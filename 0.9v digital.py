2f33c94c0eeb0af4fa78867901fa5ce58d437082
jnre <josephng1993@gmail.com>
2016-08-26 14:41:48 +0800

    Update 0.9v digital.py

--- 0.9v digital.py
+++ 0.9v digital.py
@@ -2,10 +2,10 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(18,GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(11,GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
 while True:
-    input_value = GPIO.input(18)
+    input_value = GPIO.input(11) #raw pin no
     print str(input_value)
     if input_value == 0:
         print "low"
